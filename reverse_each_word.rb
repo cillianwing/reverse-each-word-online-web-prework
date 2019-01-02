3098a0e98c1369f82f16a3824dd18539bf236186
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-02 16:45:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  new_array = []
-  sentence_array.each do |sub_array|
-    new_array << sub_array.reverse!
+  sentence_array.collect do |sub_array|
+    sub_array.reverse!
   end
   new_array.join(" ")
 end
